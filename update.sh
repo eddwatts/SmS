937184eccb6e71736a41503ded070588eb040ed4
eddwatts <edd@eddtech.co.uk>
2018-12-07 15:14:59 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,13 +14,15 @@ chmod +x /home/pi/tempnet.py
 
 
 if [[ $mode == *"kiosk"* ]]; then
-  sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
-  sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
-  chromium-browser --noerrdialogs --incognito --kiosk $url
+sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
+chromium-browser --noerrdialogs --incognito --kiosk $url
 fi 
 if [[ $mode == *"cctv"* ]]; then
-  curl -o "/home/pi/cctv.sh" "$url" -L
-  chmod +x /home/pi/cctv.sh
-  sudo /home/pi/cctv.sh stop
-  sudo /home/pi/cctv.sh repair
+echo $url
+echo '-o "/home/pi/cctv.sh" "$url" -L'
+curl -o "/home/pi/cctv.sh" "$url" -L
+chmod +x /home/pi/cctv.sh
+sudo /home/pi/cctv.sh stop
+sudo /home/pi/cctv.sh repair
 fi
