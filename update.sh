71622c147f92d19d59b64d054a85d3f53d003f13
eddwatts <edd@eddtech.co.uk>
2018-12-07 08:27:03 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,13 +11,16 @@ done
 curl -o "/home/pi/tempnet.py" "https://github.com/eddwatts/SmS/raw/master/tempnet.py" -L
 chmod +x /home/pi/tempnet.py
 
-if $mode=="kiosk"
+
+if test "$mode" = 'kiosk'; then
   sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
   sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
   chromium-browser --noerrdialogs --incognito --kiosk $url
+fi
 
-if $mode=="cctv"
+if test "$mode" = 'cctv"'; then
   curl -o "/home/pi/cctv.sh" "$url" -L
   chmod +x /home/pi/cctv.sh
   sudo /home/pi/cctv.sh stop
   sudo /home/pi/cctv.sh repair
+fi
