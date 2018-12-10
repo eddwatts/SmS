5c14089885040386785223930c4556d8fbe7428c
eddwatts <edd@eddtech.co.uk>
2018-12-10 14:37:00 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ chmod +x /home/pi/tempnet.py
 if [[ $mode == *"kiosk"* ]]; then
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
-chromium-browser --noerrdialogs --incognito --kiosk $url?id=$RANDOM
+chromium-browser --noerrdialogs --incognito --kiosk $url?random=$RANDOM
 fi 
 if [[ $mode == *"cctv"* ]]; then
-echo "getting file:" $url?id=$RANDOM
-curl -o "/home/pi/cctv.sh" $url?id=$RANDOM -L
+echo "getting file:" $url?random=$RANDOM
+curl -o "/home/pi/cctv.sh" $url?random=$RANDOM -L
 chmod +x /home/pi/cctv.sh
 sudo /home/pi/cctv.sh stop
 sudo /home/pi/cctv.sh repair
