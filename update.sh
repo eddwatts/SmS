f178f5a28e5cdaa93d3075c97d6e29b5f1f472ae
eddwatts <edd@eddtech.co.uk>
2018-12-10 12:33:07 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
+printf "Waiting for Network"
+while true; do
+    LC_ALL=C nmcli -t -f DEVICE,STATE dev | grep -q ":connected$"
+    if [ $? -eq 0 ]; then
+        break
+    else
+        sleep 1
+    fi
+done
 curl -o "/home/pi/run.py" "https://raw.githubusercontent.com/eddwatts/SmS/master/run.py" -L
 chmod +x /home/pi/run.py
 sudo ./run.sh
