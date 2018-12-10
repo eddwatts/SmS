c6f02f27f617305c5fb3e461063a11d5fafbbd00
eddwatts <edd@eddtech.co.uk>
2018-12-10 12:34:03 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 source /boot/SmS.cfg
 IFS="$IFS"$'\r'
-printf "Waiting for Network"
-while true; do
-    LC_ALL=C nmcli -t -f DEVICE,STATE dev | grep -q ":connected$"
-    if [ $? -eq 0 ]; then
-        break
-    else
-        sleep 1
-    fi
-done
+curl -o "/home/pi/update.py" "https://raw.githubusercontent.com/eddwatts/SmS/master/update.py" -L
+chmod +x /home/pi/run.py
 curl -o "/home/pi/tempnet.py" "https://raw.githubusercontent.com/eddwatts/SmS/master/tempnet.py" -L
 chmod +x /home/pi/tempnet.py
-
-
 if [[ $mode == *"kiosk"* ]]; then
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
