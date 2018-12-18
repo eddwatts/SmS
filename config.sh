a956d174ab1151638486fadae94c469ac50104e4
eddwatts <edd@eddtech.co.uk>
2018-12-18 10:25:02 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,26 @@
-echo "768 or 1080: "
-read res
-echo "type hostname for this device: "
-read hostname
-echo "set password for this device: "
-read mypass
+read -p "clearing buffer 10" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 9" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 8" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 7" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 6" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 5" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 4" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 3" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 2" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "clearing buffer 1" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "768 or 1080: " res
+read -p "type hostname for this device: " hostname
+read -p "set password for this device: " mypass
 IFS="$IFS"$'\r'
 if [[ $res == *"768"* ]]; then
 echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
