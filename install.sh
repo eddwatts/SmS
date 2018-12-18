8b970c3574be32fc9ae71a4f8c22a75456c3eb1a
eddwatts <edd@eddtech.co.uk>
2018-12-18 10:41:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,20 +53,19 @@ read -t 1 -n 100000 discard
 read -t 1 -n 100000 discard 
 read -t 1 -n 100000 discard 
 read -t 1 -n 100000 discard 
-#echo "768 or 1080: "
-#read res
-#echo "type hostname for this device: "
-#read hostname
-#echo "set password for this device: "
-#read mypass
-#IFS="$IFS"$'\r'
-#if [[ $res == *"768"* ]]; then
-#echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
-#echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
-#fi
-#if [[ $res == *"1080"* ]]; then
-#echo 'hdmi_group=1' | sudo tee --append /boot/config.txt
-#echo 'hdmi_mode=31' | sudo tee --append /boot/config.txt
-#fi
-#sudo raspi-config nonint do_hostname $hostname
-#sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
+read -p "clearing buffer" -t 1 -n 10000 discard
+echo -ne "\033c"
+read -p "768 or 1080: " res
+read -p "type hostname for this device: " hostname
+read -p "password for this device: " mypass
+IFS="$IFS"$'\r'
+if [[ $res == *"768"* ]]; then
+echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
+echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
+fi
+if [[ $res == *"1080"* ]]; then
+echo 'hdmi_group=1' | sudo tee --append /boot/config.txt
+echo 'hdmi_mode=31' | sudo tee --append /boot/config.txt
+fi
+sudo raspi-config nonint do_hostname $hostname
+sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
