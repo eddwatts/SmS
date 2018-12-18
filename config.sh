3c34857f3a66aa397a028d5290b44942bc248819
eddwatts <edd@eddtech.co.uk>
2018-12-18 09:49:35 +0000

    Update and rename installfix.sh to config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 echo "768 or 1080: "
 read res
 echo "type hostname for this device: "
@@ -6,13 +5,6 @@ read hostname
 echo "set password for this device: "
 read mypass
 IFS="$IFS"$'\r'
-sudo raspi-config nonint do_memory_split 256
-sudo raspi-config nonint do_boot_wait 0
-sudo raspi-config nonint do_camera 0
-sudo raspi-config nonint do_spi 0
-sudo raspi-config nonint do_i2c 0
-sudo raspi-config nonint do_serial 0
-sudo raspi-config nonint do_onewire 0
 if [[ $res == *"768"* ]]; then
 echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
 echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
