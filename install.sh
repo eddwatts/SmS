460be07972dc06a2f6e59b1f6e98ea4487d7eea6
eddwatts <edd@eddtech.co.uk>
2018-12-18 09:48:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,20 +43,20 @@ sudo apt-get install unclutter screen omxplayer i2c-tools -y
 sudo apt-get purge piwiz idle3 java-common geany -y
 sudo apt-get clean
 sudo apt-get autoremove -y
-echo "768 or 1080: "
-read res
-echo "type hostname for this device: "
-read hostname
-echo "set password for this device: "
-read mypass
-IFS="$IFS"$'\r'
-if [[ $res == *"768"* ]]; then
-echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
-echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
-fi
-if [[ $res == *"1080"* ]]; then
-echo 'hdmi_group=1' | sudo tee --append /boot/config.txt
-echo 'hdmi_mode=31' | sudo tee --append /boot/config.txt
-fi
-sudo raspi-config nonint do_hostname $hostname
-sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
+#echo "768 or 1080: "
+#read res
+#echo "type hostname for this device: "
+#read hostname
+#echo "set password for this device: "
+#read mypass
+#IFS="$IFS"$'\r'
+#if [[ $res == *"768"* ]]; then
+#echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
+#echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
+#fi
+#if [[ $res == *"1080"* ]]; then
+#echo 'hdmi_group=1' | sudo tee --append /boot/config.txt
+#echo 'hdmi_mode=31' | sudo tee --append /boot/config.txt
+#fi
+#sudo raspi-config nonint do_hostname $hostname
+#sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
