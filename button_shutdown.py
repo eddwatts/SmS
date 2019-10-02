1191b9c1b4bdd731f4825199a942796c5e2146ee
eddwatts <edd@eddtech.co.uk>
2019-10-02 15:53:32 +0100

    15 seconds shutdown pi

--- button_shutdown.py
+++ button_shutdown.py
@@ -10,11 +10,12 @@ def shutdown():
     check_call(['sudo', 'poweroff'])
 
 def screenon();
-    check_call(['sudo', 'echo on 0 | cec-client -s -d 1'])
-    relay.on()
-    time.sleep(60)
-    check_call(['sudo', 'echo standby 0 | cec-client -s -d 1'])
-    relay.off()
+    if not relay.is_lit:
+        check_call(['sudo', 'echo on 0 | cec-client -s -d 1'])
+        relay.on()
+        time.sleep(60)
+        check_call(['sudo', 'echo standby 0 | cec-client -s -d 1'])
+        relay.off()
 
 def screentog();
     relay.toggle()
@@ -22,10 +23,10 @@ def screentog();
 shutdown_btn = Button(17, hold_time=15)
 shutdown_btn.when_held = shutdown
 
-screentog_btn = Button(17, hold_time=10)
+screentog_btn = Button(17, hold_time=5)
 screentog_btn.when_held = screentog
 
-screenon_btn = Button(17, hold_time=5)
+screenon_btn = Button(17, hold_time=2)
 screenon_btn.when_held = screenon
 
 pause()
