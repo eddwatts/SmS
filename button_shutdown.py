105c5ce5294b6d7dcd2cb4c33a674378d34a2d92
eddwatts <edd@eddtech.co.uk>
2019-10-02 15:39:29 +0100

    Update button_shutdown.py

--- button_shutdown.py
+++ button_shutdown.py
@@ -1,20 +1,31 @@
 from gpiozero import Button
+from gpiozero import LED
 from subprocess import check_call
 from signal import pause
 import time
+
+relay = LED(27)
 
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
 def screenon();
     check_call(['sudo', 'echo on 0 | cec-client -s -d 1'])
+    relay.on()
     time.sleep(60)
     check_call(['sudo', 'echo standby 0 | cec-client -s -d 1'])
+    relay.off()
+
+def screentog();
+    relay.toggle()
     
-shutdown_btn = Button(17, hold_time=10)
+shutdown_btn = Button(17, hold_time=15)
 shutdown_btn.when_held = shutdown
 
-screenon_btn = Button(17, hold_time=2)
+screentog_btn = Button(17, hold_time=10)
+screentog_btn.when_held = screentog
+
+screenon_btn = Button(17, hold_time=5)
 screenon_btn.when_held = screenon
 
 pause()
