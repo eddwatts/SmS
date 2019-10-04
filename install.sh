751f86a1a1933ff394fda315944819b5581cc70c
eddwatts <edd@eddtech.co.uk>
2019-10-04 11:09:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,10 +69,13 @@ IFS="$IFS"$'\r'
 if [[ $res == *"768"* ]]; then
 echo 'hdmi_group=2' | sudo tee --append /boot/config.txt
 echo 'hdmi_mode=16' | sudo tee --append /boot/config.txt
+echo 'disable_overscan=1' | sudo tee --append /boot/config.txt
 fi
 if [[ $res == *"1080"* ]]; then
 echo 'hdmi_group=1' | sudo tee --append /boot/config.txt
 echo 'hdmi_mode=31' | sudo tee --append /boot/config.txt
+echo 'disable_overscan=1' | sudo tee --append /boot/config.txt
+
 fi
 sudo raspi-config nonint do_hostname $hostname
 sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
