14574fa74d3a423eb7a293527343d2e45f8d153e
eddwatts <edd@eddtech.co.uk>
2019-10-04 13:03:02 +0100

    Create tvon.py

---
+++ tvon.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from gpiozero import Button
+from gpiozero import LED
+from subprocess import check_call
+from signal import pause
+import time
+relay = LED(27)
+relay.on()
+check_call(['sudo', 'echo on 0 | cec-client -s -d 1'])
