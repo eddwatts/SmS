2135bf4c9d053bbf6d1d476051e88d0984a75872
eddwatts <edd@eddtech.co.uk>
2020-06-05 14:32:41 +0100

    Update quicksetup.sh

--- quicksetup.sh
+++ quicksetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "clearing buffer" -t 1 -n 10000 discard
 echo -ne "\033c"
-#read -p "type hostname for this device: " hostname
-#read -p "password for this device: " mypass
+read -p "type hostname for this device: " hostname
+read -p "password for this device: " mypass
 read -p "remote.it Username: " rituser
 read -p "remote.it Password: " ritpass
 sudo sed -i 's/console=tty1/console=tty3 loglevel=3 logo.nologo/' /boot/cmdline.txt
@@ -25,6 +25,6 @@ sudo remoteit signin $rituser $ritpass
 sudo remoteit setup $hostname
 sudo remoteit add SSL 22 -t SSH
 sudo sudo remoteit add "remoteit Admin Panel" 29999 -t 7
-#sudo raspi-config nonint do_hostname $hostname
-#sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
-#sudo reboot
+sudo raspi-config nonint do_hostname $hostname
+sudo echo -e "raspberry\n$mypass\n$mypass" | passwd
+sudo reboot
