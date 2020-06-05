3fc91517b9995d89c550c6362230c1a191c1a5b9
eddwatts <edd@eddtech.co.uk>
2020-06-05 15:50:29 +0100

    Update rtspcam-install.sh

--- rtspcam-install.sh
+++ rtspcam-install.sh
@@ -9,6 +9,7 @@ read -p "Your email for letsencript renewals: " myemail
 read -p "changeip.com Domain: " mydom
 read -p "changeip.com Username: " mydomuser
 read -p "changeip.com Password: " mydompass
+echo -ne "\033c"
 sudo sed -i 's/console=tty1/console=tty3 loglevel=3 logo.nologo/' /boot/cmdline.txt
 sudo sed -i -e "s/BOOT_UART=0/BOOT_UART=1/" /boot/bootcode.bin
 echo '#dtoverlay=vc4-kms-v3d' | sudo tee --append /boot/config.txt
@@ -28,8 +29,10 @@ sudo systemctl disable vncserver-x11-serviced.service
 sudo mkdir -p /etc/motioneye && sudo mkdir -p /var/lib/motioneye
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq ddclient < /dev/null > /dev/null
 sudo apt-get install -y remoteit
-sudo remoteit signin $rituser $rituser
+sudo remoteit signin $rituser $ritpass
+echo -ne "\033c"
 sudo remoteit setup $hostname
+echo -ne "\033c"
 sudo remoteit add SSL 22 -t SSH
 sudo sudo remoteit add "remoteit Admin Panel" 29999 -t 7
 sudo apt-get install -y python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev ffmpeg git libmariadb3 libpq5 libmicrohttpd12 libio-socket-ssl-perl
@@ -46,6 +49,7 @@ echo '  password='$mydompass | sudo tee --append /etc/ddclient.conf
 echo '  #specify the domain to update' | sudo tee --append /etc/ddclient.conf
 echo '  '$mydom | sudo tee --append /etc/ddclient.conf
 sudo sed -i -e 's/run_daemon="false"/run_daemon="true"/' /etc/default/ddclient
+echo -ne "\033c"
 sudo ddclient -debug -noquiet
 sudo service ddclient start
 wget https://github.com/Motion-Project/motion/releases/download/release-4.2.2/pi_buster_motion_4.2.2-1_armhf.deb
@@ -55,6 +59,7 @@ sudo apt-get install -y apache2 python-certbot-apache
 echo 'rsa-key-size = 4096' | sudo tee --append /etc/letsencrypt/cli.ini
 echo 'email = '$myemail | sudo tee --append /etc/letsencrypt/cli.ini
 echo 'domains = '$mydom | sudo tee --append /etc/letsencrypt/cli.ini
+echo -ne "\033c"
 sudo certbot --apache
 sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
 sudo cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
@@ -77,6 +82,7 @@ echo '  ProxyPass "/"  "http://127.0.0.1:8765/"' | sudo tee --append /etc/apache
 echo '  ProxyPassReverse "/"  "http://127.0.0.1:8765/"' | sudo tee --append /etc/apache2/sites-available/motioneye.conf
 echo '  </VirtualHost>' | sudo tee --append /etc/apache2/sites-available/motioneye.conf
 echo '</IfModule>' | sudo tee --append /etc/apache2/sites-available/motioneye.conf
+echo -ne "\033c"
 sudo a2ensite motioneye.conf
 sudo systemctl restart apache2
 sudo remoteit add HTTP 80 -t HTTP
