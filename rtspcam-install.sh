097b3d78b300ebdd65ece8bf4506f1219270f7ae
eddwatts <edd@eddtech.co.uk>
2020-06-05 12:56:25 +0100

    Update rtspcam-install.sh

--- rtspcam-install.sh
+++ rtspcam-install.sh
@@ -21,8 +21,7 @@ sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_serial 0
 sudo raspi-config nonint do_onewire 0
 sudo cp /usr/share/zoneinfo/Europe/London /etc/localtime
-sudo systemctl disable vncserver-x11-serviced.service
-sudo mkdir -p /etc/motioneye && mkdir -p /var/lib/motioneye && mkdir /home/pi/noip
+sudo mkdir -p /etc/motioneye && sudo mkdir -p /var/lib/motioneye
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq ddclient < /dev/null > /dev/null
 sudo apt-get install -y ffmpeg git libmariadb3 libpq5 libmicrohttpd12 tornado jinja2 jinja2 libio-socket-ssl-perl remoteit 
 sudo rm /etc/ddclient.conf
