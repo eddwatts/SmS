6df15fb227c43bd7197bdbd747486834e64c0f16
eddwatts <edd@eddtech.co.uk>
2020-09-10 15:42:06 +0100

    Update cctv-bursar.sh

--- cctv-bursar.sh
+++ cctv-bursar.sh
@@ -22,7 +22,8 @@ if [[ $mode == *"cctv"* ]]; then
 cam1="screen -dmS cam1 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"0,0,512,384\" rtsp://10.1.8.253:7447/5b85280cb07d32af7d9d5122_2 --crop 160,0,640,360 --live -n -1'"; 
 cam2="screen -dmS cam2 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"512,0,768,192\" rtsp://10.1.0.25:7447/55bb325be68320b007c92622_2 --crop 0,0,480,360 --live -n -1'"; 
 cam3="screen -dmS cam3 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"768,0,1024,192\" rtsp://10.1.0.25:7447/55bb3559e68320b007c92657_2 --crop 0,0,480,360 --live -n -1'"; 
-cam4="screen -dmS cam4 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"512,192,768,384\" rtsp://10.1.0.25:7447/55bb30a9e68320b007c925f7_2 --crop 0,0,480,360 --live -n -1'"; 
+#cam4="screen -dmS cam4 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"512,192,768,384\" rtsp://10.1.0.25:7447/55bb30a9e68320b007c925f7_2 --crop 0,0,480,360 --live -n -1'"; 
+cam4="screen -dmS cam4 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"512,192,768,384\" rtsp://10.1.0.25:7447/5f5a36a14cabb278b6996663_2 --crop 0,0,480,360 --live -n -1'"; 
 cam5="screen -dmS cam5 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"768,192,1024,384\" rtsp://10.1.0.25:7447/55ba4a45e68320b007c91741_2 --crop 0,0,480,360 --live -n -1'"; 
 cam6="screen -dmS cam6 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"0,384,512,768\" rtsp://10.1.8.253:7447/58d3d372e4b03b4107ccf2b3_2 --crop 0,0,480,360 --live -n -1'"; 
 cam7="screen -dmS cam7 sh -c 'omxplayer --avdict rtsp_transport:tcp --win \"512,384,1024,768\" rtsp://10.1.8.253:7447/5a7c0d87e4b0d6cdfb8e5d48_2 --crop 160,0,640,360 --live -n -1'"; 
