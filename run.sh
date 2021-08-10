2bc8ce34d42206eddb5f5abc03d87aa8b82eb739
eddwatts <edd@eddtech.co.uk>
2021-08-10 13:02:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,20 +9,21 @@ if [[ $mode == *"kiosk"* ]]; then
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/pi/.config/chromium/Default/Preferences
 chromium-browser --check-for-update-interval=31536000 --noerrdialogs --incognito --kiosk $url?random=$RANDOM
-fi 
+fi
 if [[ $mode == *"cctv"* ]]; then
+#read MAC </sys/class/net/$IFACE/address
 read MAC </sys/class/net/eth0/address
 MAC="${MAC//:/}"
 MAC=${MAC^^}
-echo "getting file:" $url?MAC=$MAC&random=$RANDOM
-curl -o "/home/pi/cctv.sh" $url?random=$RANDOM -L
-echo "getting file: https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1="$MAC
+#echo "getting file:" $url"?MAC="$MAC"&random="$RANDOM
+#curl -o "/home/pi/cctv.sh" $url?random=$RANDOM -L
+echo "getting File: https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1="$MAC
 curl -o "/home/pi/cctv1.sh" https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1=$MAC -L
 sed -i -e 's/\r$//' /home/pi/cctv1.sh
-chmod +x /home/pi/cctv.sh
+#chmod +x /home/pi/cctv.sh
 chmod +x /home/pi/cctv1.sh
-sudo /home/pi/cctv.sh stop
-sudo /home/pi/cctv.sh repair
+sudo /home/pi/cctv1.sh stop
+sudo /home/pi/cctv1.sh repair
 fi
 if [[ $mode == *"NewView"* ]]; then
 sudo systemctl stop displaycameras.service
