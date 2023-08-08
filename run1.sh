521a5e86884b9577397ab12704e0eccbb27d4fbc
eddwatts <edd@eddtech.co.uk>
2023-08-08 11:04:59 +0100

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -7,9 +7,10 @@ MAC="${MAC//:/}"
 MAC=${MAC^^}
 #echo "getting file:" $url"?MAC="$MAC"&random="$RANDOM
 #curl -o "/home/pi/cctv.sh" $url?random=$RANDOM -L
-wall -n "getting File: https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1="$MAC
+sudo wall -n "getting File: https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1="$MAC
 curl -o "/home/pi/update/cctv1.sh" https://staffdashboard.stmichaelsschool.co.uk/CCTV/screens.php?MAC1=$MAC -L
 sed -i -e 's/\r$//' /home/pi/update/cctv1.sh
 chmod +x /home/pi/update/cctv1.sh
 /home/pi/update/cctv1.sh stop
 #/home/pi/update/cctv1.sh repair
+sudo wall -n "Screems will be starting soon"
